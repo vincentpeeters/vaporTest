6920f57507b68093a7cdf7f6ca936f99a72e5140
Vincent <vincent.inaki.peeters@gmail.com>
2019-12-23 11:09:49 +0100

    'created projectName from template https://github.com/vaporberlin/my-first-controller'

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,13 @@
+import FluentSQLite
+import Vapor
+
+final class User: SQLiteModel {
+    var id: Int?
+    var username: String
+    init(id: Int? = nil, username: String) {
+        self.id = id
+        self.username = username
+    }
+}
+extension User: Content {}
+extension User: Migration {}
---
+++ Sources/App/configure.swift
@@ -0,0 +1,30 @@
+import Vapor
+import Leaf
+import FluentSQLite
+
+/// Called before your application initializes.
+public func configure(
+    _ config: inout Config,
+    _ env: inout Environment,
+    _ services: inout Services
+) throws {
+
+    /// Register routes to the router
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+
+    let leafProvider = LeafProvider()
+    try services.register(leafProvider)
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
+
+    try services.register(FluentSQLiteProvider())
+
+    var databases = DatabasesConfig()
+    try databases.add(database: SQLiteDatabase(storage: .memory), as: .sqlite)
+    services.register(databases)
+
+    var migrations = MigrationConfig()
+    migrations.add(model: User.self, database: .sqlite)
+    services.register(migrations)
+}
